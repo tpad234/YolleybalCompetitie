a626871919b5ccfa597d95e4bacd5c1f0b793e66
tim diender <tim.diender@student.fontys.nl>
2022-10-09 17:22:16 +0200

    1e check

---
+++ ClassLibrary/Gebruiker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Gebruiker
+    {
+        string naam { get; }
+        string email { get; }
+        string pasword { get; }
+
+
+    }
+}
